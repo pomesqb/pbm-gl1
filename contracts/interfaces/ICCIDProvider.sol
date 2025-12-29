104d1643a9fdf317dfe5cb5c2d73df75e9b0af37
pomesqb <pomesqb@gmail.com>
2025-12-29 23:19:39 +0800

    Add programmable compliance 的應用論述 by email

--- contracts/interfaces/ICCIDProvider.sol
+++ contracts/interfaces/ICCIDProvider.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 /**
  * @title ICCIDProvider
  * @notice GL1 跨鏈身份提供者介面
- * @dev 符合 GL1 CCID 標準的身份驗證介面
+ * @dev 符合 GL1 CCID 標準的身份驗證介面，包含台灣監管要求的身份標籤功能
  */
 interface ICCIDProvider {
     /**
@@ -31,4 +31,27 @@ interface ICCIDProvider {
      * @return 是否已過期
      */
     function isCredentialExpired(address account) external view returns (bool);
+    
+    /**
+     * @notice 獲取帳戶的身份標籤
+     * @param account 帳戶地址
+     * @return 身份標籤 (RESIDENT, NON_RESIDENT, CORPORATE, SANCTIONED)
+     */
+    function getIdentityTag(address account) external view returns (bytes32);
+    
+    /**
+     * @notice 檢查帳戶是否為非居民（外國人）
+     * @dev 用於外匯額度檢查
+     * @param account 帳戶地址
+     * @return 是否為非居民
+     */
+    function isNonResident(address account) external view returns (bool);
+    
+    /**
+     * @notice 檢查帳戶是否在制裁名單（黑名單）
+     * @dev 用於 AML/制裁檢查
+     * @param account 帳戶地址
+     * @return 是否在制裁名單中
+     */
+    function isSanctioned(address account) external view returns (bool);
 }
